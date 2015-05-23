bba6af61f09055095837d99105612c7dc7452561
Ryan <crazyredevil@gmail.com>
2015-05-23 13:42:11 +0800

    Add http server for debugging

--- app.js
+++ app.js
@@ -1,5 +1,40 @@
 var application = require("./app/application.js");
+var express = require('express');
+var app = express();
 
 application.start(function(err){
-	
+	if(!err) {
+		//Handle playerAction
+		app.get("/playerAction", function(req, res){
+			var userId = parseInt(req.query.userId);
+			var action = req.query.action;
+			if(!userId || !action) {
+				res.status(400).send("Invalid query string")
+			} else {
+				application.emit("playerAction", userId, action);
+				res.send("Command success");
+			}
+		});
+
+		//Handle queryUserAchievements
+		app.get("/queryUserAchievements", function(req, res){
+			var userId = parseInt(req.query.userId);
+			if(!userId){
+				res.status(400).send("Invalid query string")
+			} else {
+				application.getModule("achievementModule").queryAllUserAchievements(userId, function(err, docs){
+					if(!err){
+						res.send(docs);
+					} else {
+						res.status(400).send("Bad Request")
+					}
+				})
+			}
+		})
+		var server = app.listen(8090, function(){
+			var host = server.address().address;
+			var port = server.address().port;
+			console.log("server have listened to http://%s:%s", host, port);
+		});
+	}
 });
